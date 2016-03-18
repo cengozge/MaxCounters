b27f0564eeaa40236e8b7bee99b7b5e0ca434244
ozge.gokay <gokayozge@gmail.com>
2016-03-18 15:45:12 +0200

    first version

---
+++ src/MaxCounters.java
@@ -0,0 +1,35 @@
+import java.util.Arrays;
+import java.util.function.IntUnaryOperator;
+
+import numericalOperations.NumericalOperations;
+
+
+public class MaxCounters {
+
+	public static void main(String[] args) {
+		int[] A = {3,4,4,6,1,4,4};
+		System.out.println(Arrays.toString(solution(5, A)));
+	}
+	
+	public static int[] solution(int N, int[] A){
+		
+		int[] counterArray = new int[N];
+		
+		for (int i = 0; i < A.length; i++) {
+			
+			int indexOfCounterArray = A[i];
+			if(1 <= A[i] && A[i] <= N ){
+				counterArray[indexOfCounterArray-1]++;
+			}
+			else{
+				int max = NumericalOperations.findMaxOfArray(counterArray);
+				Arrays.fill(counterArray, max);
+			}
+			
+		}
+		
+		return counterArray;
+		
+	}
+
+}
